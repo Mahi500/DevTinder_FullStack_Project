8d73f9337a570afa046d170febb34ba2f35711fc
Mahendar <mahigatyecracker500@gmail.com>
2026-07-23 17:46:12 +0530

    advanced routing technique

--- src/app.js
+++ src/app.js
@@ -4,29 +4,11 @@ const app = express()
 
 
 // This will handle GET call to /user
+// request /user /user/xyz /user/34 /user^^ will work for app.use
 app.get('/user', (req, res)=>{
     res.send({firstname : "Mahendar Reddy", lastname: "Bommu"})
 })
 
-app.post('/user', (req, res)=>{
-    res.send("Data successfully saved to database")
-})
-
-app.put('/user', (req,res)=>{
-    res.send("user data updated to database")
-})
-
-app.delete('/user', (req,res)=>{
-    res.send("User deleted successfully")
-})
-
-app.use('/test', (req, res)=>{
-   res.send("Test code from server")
-})
-
-app.use('/',(req, res)=>{
-    res.send("Main server")
-})
 
 app.listen(7777, ()=>{
     console.log("Server is successfully listening on port 7777")

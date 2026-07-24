8164ac2835aed4ebe7acbc36a3e6c1cfca1cda7d
Mahendar <mahigatyecracker500@gmail.com>
2026-07-24 17:13:13 +0530

    next() function with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,31 +2,13 @@ const express = require('express')
 
 const app = express()
 
-
-// This will handle GET call to /user
-// request /user /user/xyz /user/34 /user^^ will work for app.use
-// app.get(/a/, (req, res)=>{
-//     res.send({firstname : "Mahendar Reddy", lastname: "Bommu"})
-// })
-
-// app.get(/.*fly$/, (req, res)=>{
-//     res.send({firstname : "Manoj", lastname : "Bommu"})
-// })
-
-// app.get('/user', (req, res)=>{
-//     console.log(req.query)
-//     console.log(req.query.userId, req.query.password)
-//     res.send("user data fetched successfully")
-// })
-
-app.get('/user/:userId',(req, res)=>{
-    console.log(req.params.userId)
-    res.send("request params")
-})
-
-app.get('/user/:userId/:name/:password', (req, res)=>{
-    console.log(req.params.userId, req.params.name, req.params.password)
-    res.send("Dynamic request")
+app.use('/users', (req, res, next)=>{
+    console.log("Response handler 1")
+    res.send("Response 1!!")
+    next()
+}, (req, res)=>{
+    console.log("Response handler 2")
+    res.send("Response 2!!")
 })
 
 

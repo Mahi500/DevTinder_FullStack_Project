7f81e270ca0855939d98e5a927250c925fbe6218
Mahendar <mahigatyecracker500@gmail.com>
2026-07-24 18:02:48 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,28 @@ const express = require('express')
 
 const app = express()
 
-app.use('/users', (req, res, next)=>{
+app.use('/users', [(req, res, next)=>{
     console.log("Response handler 1")
-    res.send("Response 1!!")
+    //res.send("Response 1!!")
     next()
-}, (req, res)=>{
+}, (req, res, next)=>{
     console.log("Response handler 2")
-    res.send("Response 2!!")
-})
+    //res.send("Response 2!!")
+    next()
+}, (req, res, next)=>{
+    console.log("Response handler 3")
+    //res.send("Response 3!!")
+    next()
+}, (req, res, next)=>{
+    console.log("Response handler 4")
+    //res.send("Response 4!!")
+    next()
+}, (req, res, next)=>{
+    console.log("Response handler 5")
+    res.send("Response 5!!")
+    //next()
+}]
+)
 
 
 app.listen(7777, ()=>{

2819b12b0e36a7de0f6f9fb29fbfb4d85b937ea5
Mahendar <mahigatyecracker500@gmail.com>
2026-08-30 23:46:42 +0530

    connection request review

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -57,5 +57,33 @@ requestRouter.post('/request/send/:status/:toUserId', userAuth , async (req, res
   
 })
 
+requestRouter.post('/request/review/:status/:requestId', userAuth, async (req, res)=>{
+  try{
+    // requestId is _id of connection requests
+    // toUserId person can only accept or reject the request
+    // status should be "interested" in connection requests collection not "ignored" 
+    const loggedInUser = req.user
+    const {status, requestId} = req.params
+    const INCLUDED_STATUS = ["accepted", "rejected"]
+    if(!INCLUDED_STATUS.includes(status)){
+     return res.status(400).json({message : "Status not valid"})
+    }
+    const connectionRequest = await ConnectionRequestModel.findOne({
+      _id : requestId,
+      toUserId : loggedInUser._id,
+      status : "interested"
+    })
+    if(!connectionRequest){
+      return res.status(400).json({message : "Connection Request not found"})
+    }
+    connectionRequest.status = status
+    const data = await connectionRequest.save()
+    res.json({message : "Connection Request " + status, data})
+  }
+  catch(error) {
+       res.status(400).send("Error:"+ error.message)
+  }
+})
+
 
 module.exports = requestRouter 
